37cf17474d65ac22376a4be560723c702b45aa24
Eric Schmidt <ericschmidt49@gmail.com>
2023-05-22 19:34:47 -0300

    Get comments and save data to json file.

--- reddit_credentials.py
+++ reddit_credentials.py
@@ -1,6 +1,5 @@
 import os
 from dotenv import load_dotenv
-import praw
 
 # Setup up IDs and variables from .env local file.
 load_dotenv("C:/Python/EnvironmentVariables/.env")
@@ -11,18 +10,3 @@ app_platform = os.environ.get("reddit_app_platform")
 reddit_username = os.environ.get("reddit_username")
 
 USER_AGENT = "windows:" + f"localhost:{app_name}:v1 (by /u/" + reddit_username + ")"
-
-# Create the Reddit instance
-reddit = praw.Reddit(client_id=app_id,
-                     client_secret=app_secret,
-                     user_agent=USER_AGENT)
-
-# Specify the subreddit to search
-subreddit = reddit.subreddit("python")
-
-# Retrieve the top posts from the subreddit
-posts = subreddit.top(limit=10)
-
-# Iterate over the posts and print their titles
-for post in posts:
-    print(post.title)
--- reddit_streamer.py
+++ reddit_streamer.py
@@ -1,7 +1,62 @@
 import reddit_credentials
+import praw
+import json
 
 app_name = reddit_credentials.app_name
 app_id = reddit_credentials.app_id
 app_secret = reddit_credentials.app_secret
 app_platform = reddit_credentials.app_platform
 reddit_username = reddit_credentials.reddit_username
+USER_AGENT = reddit_credentials.USER_AGENT
+
+# Create the Reddit instance
+reddit = praw.Reddit(client_id=app_id,
+                     client_secret=app_secret,
+                     user_agent=USER_AGENT)
+
+# Define the keyword to search for
+keyword = "python"
+
+subreddits = [
+    "careerguidance"
+]
+""" 
+Subreddits to use when we fix performance issue:
+"programming",
+"learnprogramming",
+"cscareerquestions",
+"careerguidance",
+"dailyprogrammer",
+"coding",
+"computerscience",
+"SoftwareEngineering", 
+"""
+
+nr_posts = 10
+nr_comments = 2
+posts_data = {}
+for subreddit_name in subreddits:
+    subreddit = reddit.subreddit(subreddit_name)
+    posts = subreddit.search(keyword, sort="comments", limit=nr_posts)
+    posts_data[subreddit_name] = []
+    
+    for post in posts:
+        post_data = {
+            "title": post.title,
+            "score": post.score,
+            "comments": []
+        }
+        
+        for comment in post.comments[:nr_comments]:
+            post_data["comments"].append({
+                "body": comment.body,
+                "score": comment.score
+            })
+        
+        posts_data[subreddit_name].append(post_data)
+print(posts_data)
+
+# Printing the posts_data dictionary
+with open('reddit_data.json', 'w') as json_file:
+    json.dump(posts_data, json_file, indent=4)
+
