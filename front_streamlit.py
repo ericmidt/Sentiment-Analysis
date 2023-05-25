e6977cd30512d7de51b9b5cca2d7154a1c52fda9
Eric Schmidt <ericschmidt49@gmail.com>
2023-05-25 20:24:07 -0300

    Create sidebar, buttons, data frame and bar chart.

--- front_streamlit.py
+++ front_streamlit.py
@@ -6,6 +6,10 @@ import data_preprocessing
 
 # Cancel warning in streamlit
 st.set_option('deprecation.showPyplotGlobalUse', False)
+st.title("Reddit Data Sentiment Analysis")
+# Create sidebar
+st.sidebar.header("Options")
+st.sidebar.text("Click one of the buttons to generate:")
 
 def generate_wordcloud(data, stopwords_list):
     wordcloud = WordCloud(width=800, height=400, background_color='black', max_words=100, stopwords=stopwords_list).generate(data)
@@ -16,8 +20,17 @@ def generate_wordcloud(data, stopwords_list):
     st.pyplot()
 
 def generate_chart(data):
-    pass
+    st.title("Sentiment Chart")
+    data.plot(kind='bar')
+    plt.xlabel("Sentiment")
+    plt.ylabel("Number of posts")
+    plt.show()
+    st.pyplot()
 
+def generate_dataframe(data):
+    st.title("Data Frame")
+    st.dataframe(data)
+    st.pyplot()
 
 stopwords = STOPWORDS
 stopwords.add("ive")
@@ -26,14 +39,21 @@ stopwords.add("def")
 stopwords.add("return")
 stopwords.add("python")
 stopwords.add("dont")
-stopwords.add("u") 
+stopwords.add("u")
+stopwords.add("f")
 # Assuming you have a DataFrame named 'df' with a column named 'text'
 df = pd.read_csv('posts_dataframe.csv')
+# Solve duplicate column bug
+df = df.drop('Unnamed: 0', axis=1)
 all_words = ''.join([str(string) for string in df['Posts']])
 
-""" print(df["Insight"].value_counts()) """
+sentiment_data = df["Insight"].value_counts()
 
+if st.sidebar.button("Sentiment Chart"):
+    generate_chart(sentiment_data)
 
-if st.button("Generate Word Cloud"):
+if st.sidebar.button("Word Cloud"):
     generate_wordcloud(all_words, stopwords)
 
+if st.sidebar.button("Data frame"):
+    generate_dataframe(df)
