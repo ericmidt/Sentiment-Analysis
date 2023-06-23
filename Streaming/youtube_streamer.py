1fe8792dd63c0fd214c6b67da08ad12fbc0c6fe4
Eric Schmidt <ericschmidt49@gmail.com>
2023-06-23 11:04:44 -0300

    Networking fix and save user input. Bug: only works first time.

--- Streaming/youtube_streamer.py
+++ Streaming/youtube_streamer.py
@@ -1,112 +1,59 @@
 # from dotenv import load_dotenv
+import os
 from googleapiclient.discovery import build
 import json
-from flask import Flask, request
-import os
 import requests
+import time
 
-app = Flask(__name__)
+filepath = '/app/Shared/channel_name.txt'
+while not os.path.exists(filepath):
+    print("Waiting channel name...")
+    time.sleep(1)
 
-def get_comments(channel_name):
+print("Channel name found... \nProcessing...")
+# api_key = os.getenv("youtube_api_key")
+api_key = "AIzaSyBr0k4vrmqosOglckneJBoIOVQQ2qezDw8"
 
-    api_key = "AIzaSyDVfnlpeKf_VCKrBzMbslw-uvGy9HAm7HQ"
+# Set up the API client
+youtube = build('youtube', 'v3', developerKey=api_key)
 
-    # Set up the API client
-    youtube = build('youtube', 'v3', developerKey=api_key)
+with open(filepath, 'r') as file:
+    custom_username = file.read()
 
-    # Get channel ID from custom username
-    custom_username = channel_name
-    response = youtube.search().list(part='id', q=custom_username, type='channel').execute()
-    total_results = response['pageInfo']['totalResults']
-    # If there are no search results for the channel name
-    if total_results <=0:
-        return -1
-    
-    # Extract the channel ID from the response
-    channel_id = response['items'][0]['id']['channelId']
+# Get channel ID from custom username
+response = youtube.search().list(part='id', q=custom_username, type='channel').execute()
 
-    # Get the last 10 videos of the channel
-    response = youtube.search().list(part='snippet', channelId=channel_id, maxResults=10, order='date').execute()
-    videos = response['items']
+# Extract the channel ID from the response
+channel_id = response['items'][0]['id']['channelId']
 
-    # Initialize an empty list to store the comments
-    data = []
+# Get the last 10 videos of the channel
+response = youtube.search().list(part='snippet', channelId=channel_id, maxResults=10, order='date').execute()
+videos = response['items']
 
-    # Get the top 100 comments from each video and store them in the dictionary
-    for video in videos:
-        video_id = video['id']['videoId']
-        response = youtube.commentThreads().list(part='snippet', videoId=video_id, maxResults=50, order='relevance').execute()
-        comments = [item['snippet']['topLevelComment']['snippet']['textDisplay'] for item in response['items']]
-        # Add comments to data
-        data.extend(comments)
-    return data
+# Initialize an empty dictionary to store the video titles and comments
+data = []
 
+# Get the top 100 comments from each video and store them in the dictionary
+for video in videos:
+    video_id = video['id']['videoId']
+    video_title = video['snippet']['title']
 
-def send_comments_to_data_preprocessing(data):
-    ### Uses a json file to send comments data to the Processing container's API ###
-    filename = '/app/Shared/reddit_data.json'
-    with open(filename, 'w') as json_file:
-        json.dump(data, json_file, indent=4)  # Pretty print with indent=4
-        print(f"Data saved at {filename}") 
+    response = youtube.commentThreads().list(part='snippet', videoId=video_id, maxResults=50, order='relevance').execute()
+    comments = [item['snippet']['topLevelComment']['snippet']['textDisplay'] for item in response['items']]
+    # Add comments to data
+    data.extend(comments)
 
-    url = "http://data_preprocessing:5000/app/data"
-    flask_data = {"filepath": filename}
-    response = requests.post(url=url, json=flask_data)
+### Uses a json file to send comments data to the Processing container's API ###
+filename = '/app/Shared/reddit_data.json'
+with open(filename, 'w') as json_file:
+    json.dump(data, json_file, indent=4)  # Pretty print with indent=4
+    print(f"Data saved at {filename}") 
 
-    if response.status_code == 200:
-        print("Data sent successfully to the data processing container.")
-    else:
-        print(response.status_code, " Error sending data to the data processing container.")
+url = "http://data_preprocessing:5000/app/data"
+flask_data = {"filepath": filename}
+response = requests.post(url=url, json=flask_data)
 
-
-def send_channel_name_to_streamlit(channel_name):
-    ### Uses a txt file to send the channel name to the Streamlit container's API ###
-    # Save the channel name to a .txt file
-    txt_filename = '/app/Shared/channel_name.txt'
-
-    with open(txt_filename, 'w') as txt_file:
-        txt_file.write(channel_name)
-        print(f"Channel name saved at {txt_filename}")
-
-    url = "http://front_streamlit:8501/app/channel"
-    flask_data = {"filepath": txt_filename}
-
-    # Send an HTTP POST request with the channel name file
-    response = requests.post(url=url, json=flask_data)
-
-    if response.status_code == 200:
-        print("Channel name sent successfully to the streamlit container.")
-    else:
-        print(response.status_code, " Error sending channel name to the streamlit container.")
-
-
-def teste():    
-    file_path = '/app/Shared/channel_name.json'
-    if os.path.exists(file_path):
-
-        channel_name = request.json
-        file_path = channel_name.get("filepath")
-
-        # Update the channel name in the channel_name.txt file
-        with open(file_path, 'r') as json_file:
-            data =json.load(json_file)
-
-        comments = get_comments(data)
-        if comments == -1:
-            return "Invalid channel name."
-        else:
-            send_comments_to_data_preprocessing(comments)
-            send_channel_name_to_streamlit(data)
-            return 'Channel name updated successfully.'
-
-    else:
-        comments = get_comments("fireship")
-        send_comments_to_data_preprocessing(comments)
-        send_channel_name_to_streamlit("fireship")
-        
-streamer = True
-teste()
-while streamer:
-    @app.route('/app/data', methods=['POST'])
-    def update_channel_name():
-        teste()
+if response.status_code == 200:
+    print("Data sent successfully to the data processing container.")
+else:
+    print(response.status_code, " Error sending data to the data processing container.")
--- Streamlit/front_streamlit.py
+++ Streamlit/front_streamlit.py
@@ -4,105 +4,72 @@ from wordcloud import WordCloud, STOPWORDS
 import matplotlib.pyplot as plt
 import time
 import os
-import requests
-from flask import Flask, request
 
-def load_page():
-    # Read the channel name from the .txt file
+# Cancel warning in streamlit
+st.set_option('deprecation.showPyplotGlobalUse', False)
+st.set_page_config(page_title="Youtube Sentiment Analysis")
 
-    file_path = '/app/Shared/channel_name.json'
+# Create sidebar
+st.sidebar.header("Youtube Channel Sentinment Analysis")
+st.sidebar.subheader("Options")
 
-    if os.path.exists(file_path):
+# Read the channel name from the .txt file
 
-        channel_name_file = file_path
+def generate_wordcloud(data, stopwords_list):
+    wordcloud = WordCloud(width=800, height=400, background_color='white', max_words=100, stopwords=stopwords_list).generate(data)
+    st.title("Word Cloud")
+    plt.figure(figsize=(10, 5))
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis('off')
+    st.pyplot()
 
-        with open(channel_name_file, 'r') as json_file:
+def generate_chart(data):
+    st.title("Sentiment Chart")
+    data.plot(kind='bar')
+    plt.xlabel("Sentiment")
+    plt.ylabel("Number of posts")
+    plt.show()
+    st.pyplot()
 
-            channel_name = json_file.read().strip()
+def generate_dataframe(data):
+    st.title("Comments Data Frame")
+    st.dataframe(data)
+    st.pyplot()
 
-    else:
+stopwords = STOPWORDS
+additional_stopwords = ["ive", "will", "def", "return", "python", "dont", "u", "f"]
+stopwords = list(stopwords) + additional_stopwords
 
-        print("File does not exist.")
+# Assuming you have a DataFrame named 'df' with a column named 'text'
 
-    channel_name = " dhuiashdsa "
-    # Cancel warning in streamlit
-    st.set_option('deprecation.showPyplotGlobalUse', False)
-    st.set_page_config(page_title="Youtube Sentiment Analysis")
-    st.title(f"Youtube Channel: {channel_name}")
-    # Create sidebar
-    st.sidebar.header("YouTube Channel Sentinment Analysis")
-    st.sidebar.subheader("Options")
-    
-    # Add input field for channel name
-    new_channel_name = st.sidebar.text_input(label="Enter new YouTube channel name", label_visibility="hidden", placeholder="NewChannelName")
+csv_file_path = '/app/Shared/posts_dataframe.csv'
+while not os.path.exists(csv_file_path):
+    time.sleep(120)
 
-    # Add button to trigger channel name update
-    if st.sidebar.button("Update Channel Name"):
-        # Send channel name to the appropriate container
-        url = "http://streamer:8080/app/data"
-        flask_data = {"channel_name": new_channel_name}
-        response = requests.post(url=url, json=flask_data)
+df = pd.read_csv(csv_file_path)
 
-        if response.status_code == 200:
-            st.sidebar.success("Channel name updated successfully.")
-        else:
-            st.sidebar.error("Error updating channel name.")
+# Solve duplicate column bug
+df = df.drop('Unnamed: 0', axis=1)
+all_words = ''.join([str(string) for string in df['Comments']])
 
+sentiment_data = df["Insight"].value_counts()
 
-    def generate_wordcloud(data, stopwords_list):
-        wordcloud = WordCloud(width=800, height=400, background_color='white', max_words=100, stopwords=stopwords_list).generate(data)
-        st.title("Word Cloud")
-        plt.figure(figsize=(10, 5))
-        plt.imshow(wordcloud, interpolation='bilinear')
-        plt.axis('off')
-        st.pyplot()
+if st.sidebar.button("Sentiment Chart"):
+    generate_chart(sentiment_data)
 
-    def generate_chart(data):
-        st.title("Sentiment Chart")
-        data.plot(kind='bar')
-        plt.xlabel("Sentiment")
-        plt.ylabel("Number of posts")
-        plt.show()
-        st.pyplot()
+if st.sidebar.button("Word Cloud"):
+    generate_wordcloud(all_words, stopwords)
 
-    def generate_dataframe(data):
-        st.title("Comments Data Frame")
-        st.dataframe(data)
-        st.pyplot()
-
-    stopwords = STOPWORDS
-    additional_stopwords = ["ive", "will", "def", "return", "python", "dont", "u", "f"]
-    stopwords = list(stopwords) + additional_stopwords
-
-    # Assuming you have a DataFrame named 'df' with a column named 'text'
-
-    csv_file_path = '/app/Shared/posts_dataframe.csv'
-    while not os.path.exists(csv_file_path):
-        time.sleep(120)
-
-    df = pd.read_csv(csv_file_path)
-
-    # Solve duplicate column bug
-    df = df.drop('Unnamed: 0', axis=1)
-    all_words = ''.join([str(string) for string in df['Comments']])
-
-    sentiment_data = df["Insight"].value_counts()
-
-    if st.sidebar.button("Sentiment Chart"):
-        generate_chart(sentiment_data)
-
-    if st.sidebar.button("Word Cloud"):
-        generate_wordcloud(all_words, stopwords)
-
-    if st.sidebar.button("Comments Data frame"):
-        generate_dataframe(df)
-
-    
-app = Flask(__name__)
-
-@app.route('/app/channel', methods=["POST"])
-def reload_page():
-    load_page()
+if st.sidebar.button("Comments Data frame"):
+    generate_dataframe(df)
 
 
-load_page()
+channel = st.text_input("Type Youtube Channel:")
+filepath = '/app/Shared/channel_name.txt'
+
+if st.button("Save Channel") and channel:
+
+    with open(filepath, 'w') as file:
+        file.write(channel)
+        print("Saved")
+    st.sidebar.success(f"Channel '{channel}' saved successfully!")
